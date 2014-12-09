95e5d83c0d799bfab752d778ff7e8ebb3eef9ff7
Pavel Osipenko <osipenko_pavel@ukr.net>
2014-12-09 23:15:35 +0200

    Старт

---
+++ src/ua/com/kbb/LeafMain.java
@@ -0,0 +1,10 @@
+package ua.com.kbb;
+
+public class LeafMain {
+
+	public static void main(String[] args) {
+		LeafFrames mainFrame = new LeafFrames();
+		mainFrame.setLocationRelativeTo(null);
+		mainFrame.setVisible(true);
+	}
+}
---
+++ src/ua/com/kbb/LeafSolve.java
@@ -0,0 +1,10 @@
+package ua.com.kbb;
+
+public class LeafSolve {
+
+	public static void main(String[] args) {
+		LeafFrames mainFrame = new LeafFrames();
+		mainFrame.setLocationRelativeTo(null);
+		mainFrame.setVisible(true);
+	}
+}
